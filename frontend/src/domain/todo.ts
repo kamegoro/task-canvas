e2d28d790d26035f67a7c183888d7fca2f4fce84
kamegoro <tentyo0620@icloud.com>
2025-04-21 21:26:29 +0900

    update front todo domain

--- frontend/src/domain/todo.ts
+++ frontend/src/domain/todo.ts
@@ -3,3 +3,58 @@ export type Todo = {
   content: string;
   completed: boolean;
 };
+
+class TodoId {
+  private readonly value: string;
+
+  constructor(id: string) {
+    this.value = id;
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+}
+
+class TodoContent {
+  private readonly value: string;
+
+  constructor(content: string) {
+    this.value = content;
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+}
+class TodoCompleted {
+  private readonly value: boolean;
+
+  constructor(completed: boolean) {
+    this.value = completed;
+  }
+
+  getValue(): boolean {
+    return this.value;
+  }
+}
+
+class Todo2 {
+  constructor(
+    private id: TodoId,
+    private content: TodoContent,
+    private completed: TodoCompleted,
+  ) {}
+
+  getId(): string {
+    return this.id.getValue();
+  }
+
+  getContent(): string {
+    return this.content.getValue();
+  }
+
+  getCompleted(): boolean {
+    return this.completed.getValue();
+  }
+}
