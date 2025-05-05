88d466bcb47b29940fa05440b1388faed2bd8163
kamegoro <tentyo0620@icloud.com>
2025-05-05 22:38:26 +0900

    fix frontend driver

--- frontend/src/driver/index.ts
+++ frontend/src/driver/index.ts
@@ -6,7 +6,7 @@ type DriverTodo = {
 
 type DriverTodos = DriverTodo[];
 
-export type CreateDriverRequest = {
+type CreateDriverRequest = {
   content: string;
   completed: boolean;
 };
@@ -19,105 +19,11 @@ export type UpdateDriverRequest = {
   completed: boolean;
 };
 
-export const getTodos = async (): Promise<DriverTodos> => {
-  const response = await fetch('http://localhost:3000/api/v1/todos');
-  const json = await response.json();
-
-  return json;
-};
-
-export const createTodo = async ({
-  content,
-  completed,
-}: CreateDriverRequest): Promise<CreateTodoResult> => {
-  try {
-    const response = await fetch('http://localhost:3000/api/v1/todos', {
-      method: 'POST',
-      body: JSON.stringify({
-        content: content,
-        completed: completed,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.status >= 400) {
-      throw new Error('Failed to create todo');
-    }
-
-    const json = await response.json();
-    return json.id;
-  } catch {
-    throw new Error('Failed to create todo');
-  }
-};
-
-export const updateTodoDriver = async ({
-  id,
-  content,
-  completed,
-}: UpdateDriverRequest): Promise<void> => {
-  await fetch(`http://localhost:3000/api/v1/todos/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      content: content,
-      completed: completed,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
-
-export const signIn = async (email: string, password: string): Promise<void> => {
-  const response = await fetch('http://localhost:3000/api/v1/signin', {
-    method: 'POST',
-    body: JSON.stringify({
-      email: email,
-      password: password,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (response.status !== 200) {
-    throw new Error('Failed to sign in');
-  }
-};
-
-export const signUp = async (email: string, password: string): Promise<void> => {
-  const response = await fetch('http://localhost:3000/api/v1/signup', {
-    method: 'POST',
-    body: JSON.stringify({
-      email: email,
-      password: password,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (response.status !== 200) {
-    throw new Error('Failed to sign up');
-  }
-};
-
-export const signOut = async (): Promise<void> => {
-  const response = await fetch('http://localhost:3000/api/v1/signout', {
-    method: 'POST',
-  });
-
-  if (response.status !== 200) {
-    throw new Error('Failed to sign out');
-  }
-};
-
 interface ApiRouteDriverInterface {
   getTodos(): Promise<DriverTodos>;
   createTodo(request: CreateDriverRequest): Promise<CreateTodoResult>;
   updateTodo(request: UpdateDriverRequest): Promise<void>;
+  deleteTodo(id: string): Promise<void>;
   signIn(email: string, password: string): Promise<void>;
   signUp(email: string, password: string): Promise<void>;
   signOut(): Promise<void>;
@@ -171,6 +77,12 @@ export class ApiRoutesDriver implements ApiRouteDriverInterface {
       headers: {
         'Content-Type': 'application/json',
       },
+    });
+  }
+
+  async deleteTodo(id: string): Promise<void> {
+    await fetch(`${this.baseUrl}/api/v1/todos/${id}`, {
+      method: 'DELETE',
     });
   }
 
