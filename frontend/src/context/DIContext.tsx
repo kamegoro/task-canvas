f6d8a3bdf0be46a8c6a93f76c6ea4f6d86f7133b
kamegoro <tentyo0620@icloud.com>
2025-05-05 22:54:48 +0900

    update frontend di layer

--- frontend/src/context/DIContext.tsx
+++ frontend/src/context/DIContext.tsx
@@ -1,20 +1,30 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useMemo } from 'react';
 
 import { Credential } from '@/domain/credential';
+import { RegisterTodo, Todo } from '@/domain/todo';
 import { User } from '@/domain/user';
 import { ApiRoutesDriver } from '@/driver';
 import { CredentialGateway } from '@/gateway/credential';
 import { SignOutGateway } from '@/gateway/signOutGateway';
+import TodoGateway from '@/gateway/todo';
+import { GetTodosUseCase } from '@/useCase/getTodosUseCase';
 import { SignInUseCaseImpl } from '@/useCase/signInUseCase';
 import { SignOutUseCase } from '@/useCase/signOutUseCase';
 import { SignUpUseCase } from '@/useCase/signUpUseCase';
+import { StoreTodoUseCase } from '@/useCase/storeTodoUseCase';
+import { UpdateTodoUseCase } from '@/useCase/updateTodoUseCase';
 
 type DIContainer = {
   credentialFactory: ReturnType<typeof Credential.factory>;
   userFactory: ReturnType<typeof User.factory>;
+  todoFactory: ReturnType<typeof Todo.factory>;
+  registerTodoFactory: ReturnType<typeof RegisterTodo.factory>;
   signInUseCase: SignInUseCaseImpl;
   signUpUseCase: SignInUseCaseImpl;
   signOutUseCase: SignOutUseCase;
+  getTodosUseCase: GetTodosUseCase;
+  storeTodoUseCase: StoreTodoUseCase;
+  updateTodoUseCase: UpdateTodoUseCase;
 };
 
 const DIContext = createContext<DIContainer | null>(null);
@@ -28,25 +38,38 @@ export const useDI = (): DIContainer => {
 };
 
 const DiProvider = ({ children }: { children: React.ReactNode }) => {
-  const apiRouteDriver = new ApiRoutesDriver('http://localhost:3000');
+  const diContainer = useMemo<DIContainer>(() => {
+    const apiRouteDriver = new ApiRoutesDriver('http://localhost:3000');
 
-  const credentialGateway = new CredentialGateway(apiRouteDriver);
-  const signOutGateway = new SignOutGateway(apiRouteDriver);
+    const credentialGateway = new CredentialGateway(apiRouteDriver);
+    const signOutGateway = new SignOutGateway(apiRouteDriver);
+    const todoGateway = new TodoGateway(apiRouteDriver);
 
-  const credentialFactory = Credential.factory(credentialGateway);
-  const userFactory = User.factory(signOutGateway);
+    const credentialFactory = Credential.factory(credentialGateway);
+    const userFactory = User.factory(signOutGateway);
+    const todoFactory = Todo.factory(todoGateway);
+    const registerTodoFactory = RegisterTodo.factory(todoGateway);
 
-  const signInUseCase = new SignInUseCaseImpl();
-  const signUpUseCase = new SignUpUseCase();
-  const signOutUseCase = new SignOutUseCase();
+    const signInUseCase = new SignInUseCaseImpl();
+    const signUpUseCase = new SignUpUseCase();
+    const signOutUseCase = new SignOutUseCase();
+    const getTodosUseCase = new GetTodosUseCase(todoGateway);
+    const storeTodoUseCase = new StoreTodoUseCase();
+    const updateTodoUseCase = new UpdateTodoUseCase(todoGateway);
 
-  const diContainer: DIContainer = {
-    credentialFactory,
-    userFactory,
-    signInUseCase,
-    signUpUseCase,
-    signOutUseCase,
-  };
+    return {
+      credentialFactory,
+      userFactory,
+      todoFactory,
+      registerTodoFactory,
+      signInUseCase,
+      signUpUseCase,
+      signOutUseCase,
+      getTodosUseCase,
+      storeTodoUseCase,
+      updateTodoUseCase,
+    };
+  }, []);
 
   return <DIContext.Provider value={diContainer}>{children}</DIContext.Provider>;
 };
