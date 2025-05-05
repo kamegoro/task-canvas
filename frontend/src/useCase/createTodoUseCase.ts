1adc6f790bb806905bed6647d0799e6a21af00ad
kamegoro <tentyo0620@icloud.com>
2025-05-05 22:37:57 +0900

    delete old name create todo uescase

--- frontend/src/useCase/createTodoUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { Todo } from '@/domain/todo';
-import TodoGateway from '@/gateway/todo';
-
-export const createTodo = async (content: Todo['content']): Promise<Todo> => {
-  const todoGateway = new TodoGateway();
-  const completed = false;
-
-  return await todoGateway.createTodo(content, completed);
-};
