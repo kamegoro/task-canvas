fb876becb0b9282f4d1633385c4025d0b8195392
kamegoro <tentyo0620@icloud.com>
2025-06-16 18:25:47 +0900

    add Menu to header

--- frontend/src/_components/organisms/AppHeader/index.tsx
+++ frontend/src/_components/organisms/AppHeader/index.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { useAuth0 } from '@auth0/auth0-react';
+import { useRouter } from 'next/navigation';
 
-const AppHeader: React.FC = () => {
-  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+import MenuIcon from '@mui/icons-material/Menu';
+
+import IconButton from '@/_components/mui/IconButton';
+import Menu from '@/_components/mui/Menu';
+import MenuItem from '@/_components/mui/MenuItem';
+import { useSignOut } from '@/hooks/useSignOut';
+import Box from '@/_components/mui/Box';
+
+const AppHeader: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
+  const router = useRouter();
+  const { execute } = useSignOut();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <header>
-      {isAuthenticated && user ? (
-        <>
-          <button onClick={() => logout()}>Logout</button>
-          <div>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-          </div>
-        </>
-      ) : (
-        <button onClick={() => loginWithRedirect()}>Login</button>
-      )}
-    </header>
+    <Box
+      sx={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}
+    >
+      <IconButton
+        id="header-menu-button"
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        aria-controls={open ? 'header-menu' : undefined}
+        onClick={handleClick}
+      >
+        <MenuIcon sx={{ color: 'black' }} />
+      </IconButton>
+      <Menu
+        id="header-menu"
+        open={open}
+        onClose={handleClose}
+        anchorEl={anchorEl}
+      >
+        <MenuItem
+          onClick={() => {
+            execute();
+            router.push('/signin');
+            handleClose();
+          }}
+        >
+          ログアウト
+        </MenuItem>
+      </Menu>
+      {children}
+    </Box>
   );
 };
 
