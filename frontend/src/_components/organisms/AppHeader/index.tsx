7863ab7f007a5852021c1006236a34a08160d0d7
kamegoro <tentyo0620@icloud.com>
2025-06-26 08:26:42 +0900

    update package and header component

--- frontend/src/_components/organisms/AppHeader/index.tsx
+++ frontend/src/_components/organisms/AppHeader/index.tsx
@@ -9,12 +9,16 @@ import IconButton from '@/_components/mui/IconButton';
 import Menu from '@/_components/mui/Menu';
 import MenuItem from '@/_components/mui/MenuItem';
 import { useSignOut } from '@/hooks/useSignOut';
+import { useUser } from '@/hooks/useUser';
 
 const AppHeader: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const open = Boolean(anchorEl);
   const router = useRouter();
+  const { user } = useUser();
   const { execute } = useSignOut();
+
+  if (!user) return null;
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -28,36 +32,45 @@ const AppHeader: React.FC<React.PropsWithChildren> = ({ children }) => {
     <Box
       sx={{
         width: '100%',
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
+        backgroundColor: '#fafafa',
       }}
     >
-      <IconButton
-        id="header-menu-button"
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        aria-controls={open ? 'header-menu' : undefined}
-        onClick={handleClick}
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          padding: '4px 0',
+        }}
       >
-        <MenuIcon sx={{ color: 'black' }} />
-      </IconButton>
-      <Menu
-        id="header-menu"
-        open={open}
-        onClose={handleClose}
-        anchorEl={anchorEl}
-      >
-        <MenuItem
-          onClick={() => {
-            execute();
-            router.push('/signin');
-            handleClose();
-          }}
+        <IconButton
+          id="header-menu-button"
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          aria-controls={open ? 'header-menu' : undefined}
+          onClick={handleClick}
+          sx={{ marginRight: 1 }}
         >
-          ログアウト
-        </MenuItem>
-      </Menu>
+          <MenuIcon sx={{ color: 'black', height: 32, width: 32 }} />
+        </IconButton>
+        <Menu
+          id="header-menu"
+          open={open}
+          onClose={handleClose}
+          anchorEl={anchorEl}
+        >
+          <MenuItem
+            onClick={() => {
+              execute();
+              router.push('/signin');
+              handleClose();
+            }}
+          >
+            ログアウト
+          </MenuItem>
+        </Menu>
+      </Box>
       {children}
     </Box>
   );
