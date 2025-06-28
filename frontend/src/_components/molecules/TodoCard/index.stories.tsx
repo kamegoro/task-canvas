03c4697ca08fffb7101f1d1d2c7024d81dbaf71f
kamegoro <tentyo0620@icloud.com>
2025-06-29 07:42:26 +0900

    exec storybook upgrade

--- frontend/src/_components/molecules/TodoCard/index.stories.tsx
+++ frontend/src/_components/molecules/TodoCard/index.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/nextjs';
 
 import TodoCard, { TodoCardProps } from '.';
 
--- frontend/src/_components/mui/Dialog/index.stories.tsx
+++ frontend/src/_components/mui/Dialog/index.stories.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/nextjs';
 
 import Button from '@/_components/mui/Button';
 
